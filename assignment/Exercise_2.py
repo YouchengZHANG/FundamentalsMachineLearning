fba7cf1bb5314b0d896bd7d36afccb8a65fd629f
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2020-12-01 13:56:50 +0100

    Add files via upload

--- assignment/Exercise_2.py
+++ assignment/Exercise_2.py
@@ -7,6 +7,7 @@
 
 
 # 3 Data Preparation
+import matplotlib
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn import model_selection
@@ -75,13 +76,18 @@ predicted_labels = nearest_mean(training_features=X_train, training_labels=Y_tra
 
 
 # 4.2 Visualize the decision regions (4 points)
-def plotDecisionRegions(x,y):
+def plotDecisionRegions(x, y, model):
 
     x1_min, x1_max = x[:, 0].min() - 0.5, x[:,0].max() + 0.5
     x2_min, x2_max = x[:, 1].min() - 0.5, x[:,1].max() + 0.5
     xx1, xx2= np.meshgrid (np.arange(x1_min, x1_max,step= (x1_max-x1_min)/200), np.arange(x2_min, x2_max,step=(x2_max-x2_min)/200))
 
-    Z = nearest_mean(training_features=x, training_labels=y, test_features=np.c_[xx1.ravel(), xx2.ravel()])
+    if model == nearest_mean:
+        Z = model(training_features=x, training_labels=y, test_features=np.c_[xx1.ravel(), xx2.ravel()])
+    elif model == predict_qda:
+        mu, covmat, p = fit_qda(training_features=x, training_labels=y)
+        Z = model(mu, covmat, p, test_features=np.c_[xx1.ravel(), xx2.ravel()])
+    
     Z = Z.reshape(xx1.shape)
 
     TL = [tl for tl in set(y)]    # ==1,==7 
@@ -98,7 +104,7 @@ def plotDecisionRegions(x,y):
         plt.scatter(x=TF[TL == TL[i],0],y=TF[TL == TL[i],1], marker=mk[i], color=rgb,alpha=0.9,s=250,label=("Mean Y==" + str(TL[i])))
     pass
 
-plotDecisionRegions(x=X_train,y=Y_train)
+plotDecisionRegions(x=X_train,y=Y_train,model=nearest_mean)
 plt.scatter(x=X_test[Y_test == 1,0],y=X_test[Y_test == 1,1], marker="P", color='steelblue',alpha=0.6,s=50,label="Truth Y==1")
 plt.scatter(x=X_test[Y_test == 7,0],y=X_test[Y_test == 7,1], marker="o", color='salmon',alpha=0.6,s=50,label="Truth Y==7")
 plt.legend()
@@ -164,7 +170,59 @@ calQDAerror(true_labels=Y_test,predicted_labels=predicted_test_labels)
 
 
 # 5.3 Visualization (5 points)
+from matplotlib.patches import Ellipse
 
+def plot_ellipse(mu, covmat, c):  # splot
+    v, w = np.linalg.eigh(covmat)
+    u = w[0] / np.linalg.norm(w[0])
+    angle = np.arctan(u[1] / u[0])
+    angle = 180 * angle / np.pi  # convert to degrees
+    # filled Gaussian at 2 standard deviation
+    eps = Ellipse(mu, 2 * v[0] ** 0.5, 2 * v[1] ** 0.5,
+                              180 + angle, facecolor=c,
+                              edgecolor='gray', linewidth=2)
+    eps.set_alpha(0.2)
+    ax.add_artist(eps)
+
+
+def plotDecisionRegionsQDA(x, y, model):
+
+    x1_min, x1_max = x[:, 0].min() - 0.5, x[:,0].max() + 0.5
+    x2_min, x2_max = x[:, 1].min() - 0.5, x[:,1].max() + 0.5
+    xx1, xx2= np.meshgrid (np.arange(x1_min, x1_max,step= (x1_max-x1_min)/200), np.arange(x2_min, x2_max,step=(x2_max-x2_min)/200))
+
+    if model == nearest_mean:
+        Z = model(training_features=x, training_labels=y, test_features=np.c_[xx1.ravel(), xx2.ravel()])
+    elif model == predict_qda:
+        mu, covmat, p = fit_qda(training_features=x, training_labels=y)
+        Z = model(mu, covmat, p, test_features=np.c_[xx1.ravel(), xx2.ravel()])
+
+    Z = Z.reshape(xx1.shape)
+
+    TL = [tl for tl in set(y)]    # ==1,==7 
+    TF = np.array([np.mean(x[y==tl, : ], axis=0) for tl in TL])
+    
+    #plt.figure()
+    plt.contour(xx1, xx2, Z, cmap=plt.cm.Paired, alpha= 0.3)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    mk = [m for m in range(4,12)]
+    for i in range(len(TL)):
+        rgb = np.random.rand(3,)
+        plt.scatter(x=TF[TL == TL[i],0],y=TF[TL == TL[i],1], marker=mk[i], color=rgb,alpha=0.9,s=250,label=("Mean Y==" + str(TL[i])))
+        plot_ellipse(mu[i,:], covmat[i,], c=rgb)
+    pass
+
+
+fig, ax = plt.subplots()
+plotDecisionRegionsQDA(x=X_train,y=Y_train,model=predict_qda)
+plt.scatter(x=X_train[Y_train == 1,0],y=X_train[Y_train == 1,1], marker="P", color='steelblue',alpha=0.6,s=50,label="Truth Y==1")
+plt.scatter(x=X_train[Y_train == 7,0],y=X_train[Y_train == 7,1], marker="o", color='salmon',alpha=0.6,s=50,label="Truth Y==7")
+plt.legend()
+plt.show()
+#plt.scatter(x=X_test[Y_test == 1,0],y=X_test[Y_test == 1,1], marker="P", color='steelblue',alpha=0.6,s=50,label="Truth Y==1")
+#plt.scatter(x=X_test[Y_test == 7,0],y=X_test[Y_test == 7,1], marker="o", color='salmon',alpha=0.6,s=50,label="Truth Y==7")
 
 
 
@@ -192,7 +250,7 @@ def evaluateQDA(X,Y,k):
 evaluateQDA(X=reduced_x,Y=Y_sub,k=10)
 
 
-
+import pandas as pd
 # 6 LDA (8 points)
 # 6.1 Implement LDA Training (6 points)
 def fit_lda(training_features, training_labels):
