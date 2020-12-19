1e6874bec44892cfb552aa39fa7e48e5a295bad3
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2020-12-19 17:40:40 +0100

    Add files via upload

--- assignment/Exercise_4.py
+++ assignment/Exercise_4.py
@@ -2,13 +2,10 @@
 ## Exercise 4
 
 # 1 Comment on your and other’s solution to Exercise 3
+
+
+
 # 2 Density Tree and Decision Tree (20 points)
-
-
-
-# Leave-one-out error : https://www.statology.org/leave-one-out-cross-validation-in-python/
-# https://www.statology.org/leave-one-out-cross-validation/
-
 
 
 # Preliminaries
@@ -73,6 +70,7 @@ class DensityTree(Tree):
         self.root.data = data
         self.root.box = m.copy(), M.copy()
 
+
         # build the tree
         stack = [self.root]
         while len(stack):
@@ -82,19 +80,27 @@ class DensityTree(Tree):
                 # Call 'make_density_split_node()' with 'D_try' randomly selected 
                 # indices from 'valid_features'. This turns 'node' into a split node
                 # and returns the two children, which must be placed on the 'stack'.
-                feature_indices = np.random.choice(valid_features, size=D_try, replace=False)
-                left, right = make_density_split_node(node=node, N=n, feature_indices=feature_indices)
-                stack += [left,right]
+                valid_features = np.where(np.min(node.data, axis=0) != np.max(node.data, axis=0))[0]
+                if len(valid_features) > 0:
+                    feature_indices = np.random.choice(valid_features, size=D_try, replace=False)
+                    left, right = make_density_split_node(node=node, N=N, feature_indices=feature_indices)
+                    stack.extend([left,right])
+                else:
+                    make_density_leaf_node(node=node, N=N)
                 
             else:
                 # Call 'make_density_leaf_node()' to turn 'node' into a leaf node.
-                make_density_leaf_node(node=node, N=n)
+                make_density_leaf_node(node=node, N=N)
 
     def predict(self, x):
         leaf = self.find_leaf(x)
         # return p(x | y) * p(y) if x is within the tree's bounding box 
         # and return 0 otherwise
-        return ... # your code here
+        if (self.box[0] <= x).all() and (x <= self.box[1]).all():
+            p = leaf.response * self.prior
+        else:
+            p = 0
+        return p
 
 
 
@@ -130,9 +136,9 @@ def make_density_split_node(node, N, feature_indices):
         for t in tj:
             
             mL, ML = m.copy(), M.copy()
-            ML[:,j] = t
+            ML[j,] = t
             mR, MR = m.copy(), M.copy()
-            mR[:,j] = t
+            mR[j,] = t
 
             VL = np.prod(ML - mL)
             VR = np.prod(MR - mR)
@@ -160,8 +166,10 @@ def make_density_split_node(node, N, feature_indices):
     #####  np.shares_memory()
     # initialize 'left' and 'right' with the data subsets and bounding boxes
     # according to the optimal split found above
-    m_new, M_new = m.copy(), M.copy()
-    M_new[:,j_min], m_new[:,j_min]= t_min, t_min  # New    #m_new[:,j_min] = t_min
+    m_new = m.copy()
+    M_new = M.copy()
+    M_new[j_min,] =  t_min
+    m_new[j_min,] = t_min  # New    #m_new[:,j_min] = t_min
     left.data =  node.data[node.data[:,j_min] < t_min , :] # store data in left node -- for subsequent splits
     left.box = node.box[0].copy(), M_new.copy()  # store bounding box in left node
     right.data = node.data[node.data[:,j_min] > t_min , :]
@@ -185,8 +193,8 @@ def make_density_leaf_node(node, N):
     '''
     # compute and store leaf response
     n = node.data.shape[0]
-    v = np.prob(node.box[1] - node.box[0])
-    node.response = n/(N*v)
+    v = np.prod(node.box[1] - node.box[0])
+    node.response = n/(N*v)    # likelihood
 
 
 
@@ -204,7 +212,7 @@ class DecisionTree(Tree):
         '''
         N, D = data.shape
         D_try = int(np.sqrt(D)) # how many features to consider for each split decision
-
+ 
         # initialize the root node
         self.root.data = data
         self.root.labels = labels
@@ -217,15 +225,22 @@ class DecisionTree(Tree):
                 # Call 'make_decision_split_node()' with 'D_try' randomly selected 
                 # feature indices. This turns 'node' into a split node
                 # and returns the two children, which must be placed on the 'stack'.
-                
+
+                valid_features = np.where(np.min(node.data, axis=0) != np.max(node.data, axis=0))[0]
+                feature_indices = np.random.choice(valid_features, size=D_try, replace=False)
+                left, right = make_decision_split_node(node=node, feature_indices=feature_indices)
+                stack.extend([left,right])
+
             else:
                 # Call 'make_decision_leaf_node()' to turn 'node' into a leaf node.
-                ... # your code here
+                make_decision_leaf_node(node=node,minlength=len(set(labels)))
                 
+
     def predict(self, x):
         leaf = self.find_leaf(x)
         # compute p(y | x)
-        return ... # your code here
+        p = leaf.response / leaf.N
+        return p
 
 
 def make_decision_split_node(node, feature_indices):
@@ -247,15 +262,18 @@ def make_decision_split_node(node, feature_indices):
         # Illustration: for loop - hint: vectorized version is possible
         for t in tj:
             
-            NL = node.labels[node.data[:,j] < t].shape[0]
-            NR = node.labels[node.data[:,j] > t].shape[0]
+            NL = len(node.labels[node.data[:,j] < t])
+            NR = len(node.labels[node.data[:,j] > t])
 
-            errGL = NL*(1-sum( [(NL[NL==list(set(digits))[k]] / NL) ** 2 for k in list(set(node.labels))] ))
-            errGR = NR*(1-sum( [(NR[NR==list(set(digits))[k]] / NR) ** 2 for k in list(set(node.labels))] ))
+            NLk = node.labels[node.data[:,j] < t]
+            NRk = node.labels[node.data[:,j] > t]
 
+            errGL = NL*(1-sum( [(len(NLk[NLk==k]) / NL) ** 2 for k in list(set(node.labels))] ))
+            errGR = NR*(1-sum( [(len(NRk[NRk==k]) / NR) ** 2 for k in list(set(node.labels))] ))
+            
             # Compute the error
             gini_error = errGL + errGR
-            
+
             # choose the best threshold that
             if gini_error < e_min:
                 e_min = gini_error
@@ -268,10 +286,10 @@ def make_decision_split_node(node, feature_indices):
 
     # initialize 'left' and 'right' with the data subsets and labels
     # according to the optimal split found above
-    left.data = node.data[node.data[:,j_min] < t_min]
+    left.data = node.data[node.data[:,j_min] < t_min,:]
     left.labels = node.labels[node.data[:,j_min] < t_min]
-    right.data = node.data[node.data[:,j_min] > t_min]
-    right.labels = node.labels[node.data[:,j_min] < t_min]
+    right.data = node.data[node.data[:,j_min] > t_min,:]
+    right.labels = node.labels[node.data[:,j_min] > t_min]
 
     # turn the current 'node' into a split node
     # (store children and split condition)
@@ -285,13 +303,13 @@ def make_decision_split_node(node, feature_indices):
 
 
 
-def make_decision_leaf_node(node):
+def make_decision_leaf_node(node,minlength):
     '''
     node: the node to become a leaf
     '''
     # compute and store leaf response
-    node.N = ...
-    node.response = ... # your code here
+    node.N = len(node.data)
+    node.response = np.bincount(node.labels,minlength=minlength)
 
 
 def node_is_pure(node):
@@ -302,7 +320,122 @@ def node_is_pure(node):
 
 
 
-# Evaluation of Density and Decision Tree
+# 3 Evaluation of Density Tree and Decision Tree (6 points)
+from sklearn.datasets import load_digits
+from sklearn import model_selection
+import pandas as pd  
+import matplotlib.pyplot as plt
+
+digits = load_digits()
+data = digits["data"]
+target = digits["target"]
+target_names = digits["target_names"]
+
+prior = [len(target[target == d]) / len(target) for d in target_names]
+
+
+# 4 Density Forest and Decision Forest (8 points)
+
+# Train a generative classifier using 10 instances of DensityTree 
+
+def DSTclassifier(X_train,Y_train,X_test,Y_test,n_min,return_error):
+
+    DSTclassifier = [DensityTree() for _ in list(set(Y_train))]
+    [DSTclassifier[d].train(data=X_train[Y_train == list(set(target))[d],:], prior=prior[d], n_min=n_min) for d in range(len(set(Y_train)))]
+
+    y_test = [DSTclassifier[d].predict(X_test[i,:]) for i in range(len(X_test)) for d in range(len(set(Y_train))) ]
+    y_test = np.array(y_test).reshape([len(X_test),len(set(Y_train))])
+    y_test = np.argmax(y_test, axis=1)
+
+    if return_error == True:
+        err = (len(Y_test) - np.sum(y_test == Y_test)) / len(Y_test)
+        return y_test, err
+    else:
+        return y_test
+
+# Train a discriminative classifier using one instance of DecisionTree. 
+
+def DCTclassifier(X_train,Y_train,X_test,Y_test,n_min,return_error):
+
+    DCTclassifier = DecisionTree() 
+    DCTclassifier.train(data=X_train,labels=Y_train,n_min=n_min)
+
+    y_test = [DCTclassifier.predict(X_test[i,:]) for i in range(len(X_test))]
+    y_test = np.array(y_test).reshape([len(X_test),len(set(Y_train))])
+    y_test = np.argmax(y_test, axis=1)
+
+    if return_error == True:
+        err = (len(Y_test) - np.sum(y_test == Y_test)) / len(Y_test)
+        return y_test, err
+    else:
+        return y_test
+
+# Plot the confusion matrix
+
+X_train = X_test = data
+Y_train = Y_test = target
+yDST, errDST = DSTclassifier(X_train,Y_train,X_test,Y_test,n_min=20,return_error=True)
+yDCT, errDCT = DCTclassifier(X_train,Y_train,X_test,Y_test,n_min=20,return_error=True)
+
+confDST = pd.crosstab(pd.Series(Y_test, name='Truth'), pd.Series(yDST, name='DensityTree Predicted'))
+confDCT = pd.crosstab(pd.Series(Y_test, name='Truth'), pd.Series(yDST, name='DecisionTree Predicted'))
+
+
+# Plot the performance under various n_min
+
+n_min = [n for n in range(0,55,5)]
+
+errA,errB = [], []
+
+for n in n_min:
+    yDST, errDST = DSTclassifier(X_train,Y_train,X_test,Y_test,n_min=n,return_error=True)
+    yDCT, errDCT = DCTclassifier(X_train,Y_train,X_test,Y_test,n_min=n,return_error=True)
+    errA.append(errDST)
+    errB.append(errDCT)
+    
+plt.figure()
+plt.plot(n_min, errA, label='DensityTree')
+plt.plot(n_min, errB, label='DecisionTree')
+plt.xlabel('n_min')
+plt.ylabel('Error rate')
+plt.legend()
+
+
+# 4 Density Forest and Decision Forest (8 points)
+# Density and Decision Forest
+class DensityForest():
+    def __init__(self, n_trees):
+        # create ensemble
+        self.trees = [DensityTree() for i in range(n_trees)]
+    
+    def train(self, data, prior, n_min=20):
+        for tree in self.trees:
+            # train each tree, using a bootstrap sample of the data
+            ... # your code here
+
+    def predict(self, x):
+        # compute the ensemble prediction
+        return ... # your code here
+
+
+class DecisionForest():
+    def __init__(self, n_trees):
+        # create ensemble
+        self.trees = [DecisionTree() for i in range(n_trees)]
+    
+    def train(self, data, labels, n_min=0):
+        for tree in self.trees:
+            # train each tree, using a bootstrap sample of the data
+            ... # your code here
+
+    def predict(self, x):
+        # compute the ensemble prediction
+        return ... # your code here
+
+
+
+
+# 5 Evaluation of Density Forest and Decision Forest (6 points)
 
 
 
@@ -317,10 +450,43 @@ def node_is_pure(node):
 
 
 
+y_test.shape
+len(y_test)
+data[:,2] < 5
+target[data[:,2] < 5]
+a = [1,2,3,4,5,6,67,9]
+b = [5,6,7,7,7,8,98,1]
+a = np.array(a)
+b = np.array(b)
+
+c = a[b < 8]
+
+
+np.array([n+j for n in [1,2,3] for j in [100,1000,10000,100000]]).reshape([3,4])
+
+
+a = [1,1,1,1,1,5,5,5,5,5]
+np.array(a).reshape()
+DensityTree().train().predict(X_test[1,])
+a = DensityTree()
+b = a.train(data=X_train[Y_train == list(set(Y_train))[0],:], prior=prior[0], n_min=20)
+print(a)
+print(b)
+
+[DensityTree()] * 10
+
+np.array()
+a = np.array([1,1,1])
+b=  np.array([3,3,3])
+c = np.array([5,1,5])
 
 
 
+(a <= b).all() and (b <= c).all()
 
+a = [1,1,1]
+b=  [3,3,3]
+c = [4,1,1]
 
 digits
 
